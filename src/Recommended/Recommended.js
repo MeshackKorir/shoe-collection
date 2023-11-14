881a9a4fe2c9b4e5d1716e238fc4ad411e2b20dd
Meshack Korir <meshackkorir100@gmail.com>
2023-11-14 09:28:48 +0300

    updating

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -3,8 +3,17 @@ import "./Recommended.css"
 
 const Recommended = () => {
   return (
-    <div>Recommended</div>
-  )
+    <>
+      <h2>Recommended</h2>
+      <div className="recommende-btns">
+        <button className='btns'>All Products</button>
+        <button className='btn'>Nike</button>
+        <button className='btns'>Adidas</button>
+        <button className='btns'>Puma</button>
+        <button className='btns'>Vans</button>
+      </div>
+    </>
+ )
 }
 
 export default Recommended
