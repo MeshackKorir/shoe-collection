6168a6d50ef2d9749e9a280c58e386a7f07cb8bf
Meshack Korir <meshackkorir100@gmail.com>
2023-11-14 12:04:49 +0300

    styled Category css

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -3,7 +3,43 @@ import "./Category.css"
 
 const Category = () => {
   return (
-    <div>Category</div>
+    <h2 className='sidebar-title'>
+      
+      <div>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+            <span className='checkmark'></span>All
+        </label>
+      </div>
+
+      <div>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+            <span className='checkmark'></span>Sneakers
+        </label>
+      </div>
+
+      <div>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+            <span className='checkmark'></span>Flats
+        </label>
+      </div>
+
+      <div>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+            <span className='checkmark'></span>Sandals
+        </label>
+      </div>
+
+      <div>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+            <span className='checkmark'></span>Heels
+        </label>
+      </div>
+    </h2>
   )
 }
 
